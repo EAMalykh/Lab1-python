7e44ef291c9551a0e55a195930332c92e84e0e19
EAMalykh <malyhfistina@gmail.com>
2022-05-08 16:49:39 +0300

    Templates added

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-from flask import Flask, request, make_response, abort, redirect, render_template
+from flask import Flask, make_response, abort, redirect, render_template
 
 app = Flask(__name__)
-a = [{'name': 'Alex', 'id': 1, 'surname': 'Turner', 'age': 33},
+userslist = [{'name': 'Alex', 'id': 1, 'surname': 'Turner', 'age': 33},
 {'name': 'Thom', 'id': 2, 'surname': 'Yorke', 'age': 50},
 {'name': 'Jane', 'id': 3, 'surname': 'Porter', 'age': 17}]
 
@@ -11,12 +11,7 @@ def index():
 
 @app.route('/users')
 def userdata():
-    surnid = ""
-    for i in a:
-        s = i['surname']
-        e = i['id']
-        surnid += f'<h1><a href = "/user/{s}"> {e}. {s} </a></h1><hr style="border: 1px solid pink;">'
-    return surnid
+    return render_template('templates.html', users = userslist)
 
 @app.route('/error')
 def error():
@@ -28,10 +23,9 @@ def home():
 
 @app.route('/user/<surname>')
 def user(surname):
-    for i in a:
-        if surname == i['surname']:
-            response = make_response('<h1>Hello, %s</h1>'% i['name'])
-            return response
+    for user in userslist:
+        if surname == user['surname']:
+            return render_template('template2.html', user = user)
     return error()
 
 if __name__ == '__main__':
